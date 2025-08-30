5ee04793cb77b51b8e4843ce1c970d5ee81f1bd8
Vishud Verma <244vishudverma@gmail.com>
2025-08-30 15:28:00 +0530

    Enhance data acquisition script to include Kaggle dataset downloads and improve file handling for better organization and context

--- src/data_acquisition/01_data_acquisition.py
+++ src/data_acquisition/01_data_acquisition.py
@@ -6,17 +6,38 @@ import os
 # Source: UCI Machine Learning Repository - Diabetes 130-US hospitals for years 1999-2008 Data Set
 # Source: CDC NHANES - Demographic Variables and Sample Weights 2021-2022
 # Source: Kaggle - Hospital Admissions Dataset(India based)
+# Source: Kaggle - Pima Indians Diabetes Database
 dwnld_urls = [
     "https://archive.ics.uci.edu/static/public/296/diabetes+130-us+hospitals+for+years+1999-2008.zip",
     "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2021/DataFiles/DEMO_L.xpt"
 ]
-
-# Download of datasets from Kaggle hospital admissions data
-path = kagglehub.dataset_download("ashishsahani/hospital-admissions-data")
+kaggle_urls = [
+    "uciml/pima-indians-diabetes-database",
+    "ashishsahani/hospital-admissions-data"
+]
 
 # Download CSV files from the specified URLs
-raw_data_dir = os.path.join(os.path.dirname(__file__), "/Projects/EHR-Project/data/00_raw") # ToDo: update the path so that it works on any system
+raw_data_dir = os.path.abspath("data/00_raw")
 
+
+# Download Kaggle datasets which bring in the Indian context
+for url in kaggle_urls:
+    # Download and extract to a temp directory
+    dataset_path = kagglehub.dataset_download(url)
+    # dataset_path is the extracted folder path
+    for root, _, files in os.walk(dataset_path):
+        for file in files:
+            src_file = os.path.join(root, file)
+            dest_file = os.path.join(raw_data_dir, file)
+            if os.path.exists(dest_file):
+                print(f"{file} already exists in {raw_data_dir}, skipping copy.")
+                continue
+            os.makedirs(raw_data_dir, exist_ok=True)
+            with open(src_file, "rb") as fsrc, open(dest_file, "wb") as fdst:
+                fdst.write(fsrc.read())
+            print(f"Copied {file} to {raw_data_dir}")
+
+#Downloading the american datasets from UCI and CDC
 for url in dwnld_urls:
     filename = url.split("/")[-1]
     file_path = os.path.join(raw_data_dir, filename)
@@ -31,3 +52,6 @@ for url in dwnld_urls:
 
 
 # Note: The downloaded file is a ZIP file. You may need to extract it to access the CSV files inside. Each file being downloaded is of different format. so need to handle them individually.
+# All the files are now getting downloaded after the execution of this script to the data/00_raw folder.
+# This concludes the first step of the data pipeline - Data Acquisition.
+# The next step is Data Cleaning and Preprocessing is within the 02_data_verfication.py script.
