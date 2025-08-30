bddcc50677927f8ac180afef3bce7ab058e73293
Vishud Verma <244vishudverma@gmail.com>
2025-08-30 17:00:45 +0530

    Updated the data verification file to verify that the data is correctly downloaded and extracted and can be used for the later stages; updated the __init__.py file to accomodate the data verification functions; added one additional zipfile module to fascillitate zip extraction and usage.

--- src/data_acquisition/02_data_verification.py
+++ src/data_acquisition/02_data_verification.py
@@ -1,5 +1,30 @@
 import os
+import zipfile
 import pandas as pd
 
 #TODO: add the rest of the functions to check whether the downloaded data is of the required quality and schema, verify the completeness, and generate a report.
 
+data_path = os.path.abspath('data/00_raw') # This points to the raw data directory
+
+zip_file_path = os.path.join(data_path, 'diabetes+130-us+hospitals+for+years+1999-2008.zip')
+
+def validate_schema():
+    # Unzip the file if it exists
+    if os.path.exists(zip_file_path) and os.path.exists(os.path.join(data_path, 'diabetic_data.csv'))==False:
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(data_path)
+        print(f"Extracted {zip_file_path} to {data_path}")
+    else:
+        print(f"File is already extracted or it does not exist.")
+
+    # Example: Load datasets to verify they are correctly downloaded
+    # Adjust file names as per actual downloaded files
+    cdc_dataset = pd.read_sas(os.path.join(data_path, 'DEMO_L.xpt'))
+    diabetes_pima_dataset = pd.read_csv(os.path.join(data_path, 'diabetes.csv'))
+    india_hospital_admissions_dataset = pd.read_csv(os.path.join(data_path, 'HDHI Admission data.csv'))
+
+
+print(f"Data path is set to: {data_path}")
+validate_schema()
+
+# This verifies that all the datasets are loaded correctly and can be read using the required libraries.
--- src/data_acquisition/__init__.py
+++ src/data_acquisition/__init__.py
@@ -6,7 +6,7 @@ Designed for reproducibility and ease of use.
 """
 
 from .01_data_acquisition import  download_kaggle_datasets,download_web_datasets # type: ignore
-from .data_verification import validate_schema, check_data_quality, generate_verification_report # type: ignore
+from .02_data_verification import  validate_schema # type: ignore
 from .data_upload import save_to_raw_folder, create_mssql_connection, upload_dataframe_to_mssql, update_data_manifest # type: ignore
 
 # This allows us to write:
